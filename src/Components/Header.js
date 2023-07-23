fc077676ce3e130da232c19fbc841de44dc6a727
iampsrv <iampsrv@gmail.com>
2023-07-24 02:28:51 +0530

    fixed header and hero section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Header() {
+  // State to track whether the dropdown is open or closed
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  // Function to toggle the dropdown state
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className="bg-gray-800 text-white">
       <nav className="container mx-auto flex items-center justify-between p-4">
         <div className="text-xl font-bold">
           <h1>PS</h1>
         </div>
-        <div className="flex space-x-4">
-          <a href="#aboutme" className="hover:text-gray-300">About Me</a>
-          <a href="#workexp" className="hover:text-gray-300">Work Experience</a>
-          <a href="#services" className="hover:text-gray-300">Services</a>
-          <a href="#contactme" className="hover:text-gray-300">Contact Me</a>
+        {/* Mobile dropdown button */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleDropdown}
+            className="text-gray-300 hover:text-white focus:outline-none"
+          >
+            ☰
+          </button>
         </div>
+        {/* Desktop navigation */}
+        <div className="hidden md:flex space-x-4">
+          <a href="#aboutme" className="hover:text-gray-300">
+            About Me
+          </a>
+          <a href="#workexp" className="hover:text-gray-300">
+            Work Experience
+          </a>
+          <a href="#services" className="hover:text-gray-300">
+            Services
+          </a>
+          <a href="#contactme" className="hover:text-gray-300">
+            Contact Me
+          </a>
+        </div>
+        {/* Mobile dropdown */}
+        {isDropdownOpen && (
+          <div className="md:hidden absolute top-0 right-0 mt-12 mr-4 z-10 bg-gray-800 text-white p-4">
+            <div className="flex flex-col space-y-4">
+              <a href="#aboutme" className="hover:text-gray-300">
+                About Me
+              </a>
+              <a href="#workexp" className="hover:text-gray-300">
+                Work Experience
+              </a>
+              <a href="#services" className="hover:text-gray-300">
+                Services
+              </a>
+              <a href="#contactme" className="hover:text-gray-300">
+                Contact Me
+              </a>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import mainLogo from './profile-pic.jpg'
+import mainLogo from './profile-pic.jpg';
 
 export default function HeroSection() {
   return (
-    <div className="py-16 bg-gray-900" style={{ height: "80vh" }}>
+    <div className="py-16 bg-gray-900" style={{ minHeight: "80vh" }}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
+        <div className="lg:text-center text-center"> {/* Use text-center for center alignment */}
           <h2 className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl animate-fade-in">
             Hello, I'm Pranjal Srivastava
           </h2>
-          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-300 sm:mt-4 animate-slide-in">
-           Devops Engineer, Content Creator, Life Long Learner
-          </p>
+          <div className="mt-3 max-w-2xl mx-auto text-xl text-gray-300 sm:mt-4 animate-slide-in">
+            <div className="md:flex justify-center"> {/* Use flex and justify-center to center on larger screens */}
+              <div className="md:flex-shrink-0">DevOps Engineer</div>
+              <div className="md:flex-shrink-0 mx-2">Content Creator</div>
+              <div className="md:flex-shrink-0">Life Long Learner</div>
+            </div>
+          </div>
         </div>
         <div className="mt-10">
-          <img className="mx-auto h-48 w-48 rounded-full animate-scale-in" src={mainLogo} alt="Profile Picture" />
+          <img
+            className="mx-auto h-48 w-48 sm:h-64 sm:w-64 rounded-full animate-scale-in"
+            src={mainLogo}
+            alt="Profile Picture"
+          />
         </div>
       </div>
     </div>
